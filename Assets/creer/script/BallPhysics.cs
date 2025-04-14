6558fae14ec2b1d92acab0d1e8a9e762af178951
ebeliveau01 <2238950@carrefour.cegepvicto.ca>
2025-04-14 00:39:52 -0400

    Fixing the ball and testing the multiplayer

--- Assets/creer/script/BallPhysics.cs
+++ Assets/creer/script/BallPhysics.cs
@@ -1,55 +1,23 @@
 using UnityEngine;
 
-public class BouncingBall : MonoBehaviour
-
-{
-
+public class BouncingBall : MonoBehaviour {
     public float bounceForce = 5f;
-
     public float damping = 0.5f;
 
     private Rigidbody rb;
-
-    void Start()
-
-    {
-
+    
+    void Start() {
         rb = GetComponent<Rigidbody>();
-
     }
 
-    void Update()
-
-    {
-
-        // Simulate gravity and bounce effect
-
+    void Update() {
         if (transform.position.y <= 0.5f && rb.linearVelocity.y < 0)
-
-        {
-
-            // Apply a bounce force in the upward direction
-
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, bounceForce, rb.linearVelocity.z);
-
-        }
-
     }
 
-    void OnCollisionEnter(Collision collision)
-
-    {
-
-        // Dampen the bounce when the ball hits the ground
-
+    void OnCollisionEnter(Collision collision) {
         if (collision.relativeVelocity.y > 0)
-
-        {
-
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y * damping, rb.linearVelocity.z);
-
-        }
-
     }
 
 }
---
+++ Assets/creer/script/PointDashboard.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+
+public class PointDashboard : MonoBehaviour {
+    /// <summary>
+    /// Le nombre de point pour le joueur 1
+    /// </summary>
+    private float nbrPointJoueur1;
+
+    /// <summary>
+    /// Le nombre de point pour le joueur 2
+    /// </summary>
+    private float nbrPointJoueur2;
+
+    /// <summary>
+    /// Le composant texte qui est affiché dans l'interface
+    /// </summary>
+    private TextMeshPro pointDashboardText;
+
+    /// <summary>
+    /// Un singleton static du script
+    /// </summary>
+    public static PointDashboard Instance { get; private set; }
+
+    /// <summary>
+    /// Méthode qui s'exécute au lancement du script
+    /// </summary>
+    private void Awake() {
+        nbrPointJoueur1 = 0;
+        nbrPointJoueur2 = 0;
+
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        
+        pointDashboardText = GetComponent<TextMeshPro>();
+
+        UpdateInterfaceText();
+    }
+
+    /// <summary>
+    /// Méthode qui permet d'incrémenter le nombre de point
+    /// </summary>
+    public void IncrementerPoint(int joueur) {
+        if (joueur == 1)
+            nbrPointJoueur1++;
+        else if (joueur == 2)
+            nbrPointJoueur2++;
+        
+        UpdateInterfaceText();
+    }
+
+    private void UpdateInterfaceText() {
+        pointDashboardText.SetText($"Joueur 1 /Name/: {nbrPointJoueur1}\nJoueur 2 /Name/: {nbrPointJoueur2}");
+    }
+}
